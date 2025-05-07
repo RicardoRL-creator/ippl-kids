790130a35f3218b49c196c7ed4527ddfc18a149e
RicardoRL-creator <ricardo@rlconsultoria.net>
2025-05-07 19:51:58 +0000

    Salvando alterações antes de continuar

--- v2/src/pages/AplicacaoProvas.tsx
+++ v2/src/pages/AplicacaoProvas.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { supabase } from '../supabaseClient';
 import { format } from 'date-fns';
